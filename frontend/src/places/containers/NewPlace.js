873ed318f6d48d76df94487b599638b1db3996aa
Yevhwnii <mirandoos3223@gmail.com>
2020-08-14 19:16:14 +0300

    two way binding from child component thru useEffec

--- frontend/src/places/containers/NewPlace.js
+++ frontend/src/places/containers/NewPlace.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import "./NewPlace.css";
-import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
 import Input from "../../shared/components/FormElements/Input";
 
 const NewPlace = (props) => {
+  // useCallback reexecutes this function only when values specified in second argument change
+  // that will allow us to not stack into infinite loop because of the fact that rerendering of this function means
+  // that Input`s useEffect will be executed again and which means that this component this rerendered again and so on
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
   return (
     <form className="place-form">
       <Input
+        id="title"
         element="input"
         type="text"
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title"
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 character)"
+        onInput={descriptionInputHandler}
       />
     </form>
   );
--- frontend/src/shared/components/FormElements/Input.js
+++ frontend/src/shared/components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 
 import { validate } from "../../util/validators";
 import "./Input.css";
@@ -30,6 +30,14 @@ const Input = (props) => {
     isTouched: false,
     isValid: false,
   }); // inital values of the state
+
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
+
+  useEffect(() => {
+    onInput(id, value, isValid);
+  }, [id, value, isValid, onInput]);
+
   const changeHandler = (event) => {
     // Dispatching CHANGE action
     dispatch({
