7969b15755607c7147dfdbde81eea8aaa7ab4c32
Yevhwnii <mirandoos3223@gmail.com>
2020-08-15 18:12:59 +0300

    additional check for error prevention

--- frontend/src/shared/hooks/form-hook.js
+++ frontend/src/shared/hooks/form-hook.js
@@ -10,6 +10,10 @@ const formReducer = (state, action) => {
     case "INPUT_CHANGE":
       let formIsValid = true;
       for (const inputId in state.inputs) {
+        // Skip undefined properties
+        if (!state.inputs[inputId]) {
+          continue;
+        }
         if (inputId === action.inputId) {
           formIsValid = formIsValid && action.isValid;
         } else {
