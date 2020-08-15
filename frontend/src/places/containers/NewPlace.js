c6ad6cd7a1efab2d6292b08edae7751b9e496825
Yevhwnii <mirandoos3223@gmail.com>
2020-08-15 15:27:25 +0300

    finishing newplace for for the moment

--- frontend/src/places/containers/NewPlace.js
+++ frontend/src/places/containers/NewPlace.js
@@ -55,8 +55,14 @@ const NewPlace = (props) => {
     },
     [dispatch]
   );
+  const placeSubmitHandler = (event) => {
+    event.preventDefault();
+    // Later send requests to the server
+    console.log(formState.inputs);
+  };
+
   return (
-    <form className="place-form">
+    <form className="place-form" onSubmit={placeSubmitHandler}>
       <Input
         id="title"
         element="input"
@@ -72,6 +78,14 @@ const NewPlace = (props) => {
         label="Description"
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText="Please enter a valid description (at least 5 character)"
+        onInput={inputHandler}
+      />
+      <Input
+        id="address"
+        element="input"
+        label="Address"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid address"
         onInput={inputHandler}
       />
       <Button type="submit" disabled={!formState.isValid}>
