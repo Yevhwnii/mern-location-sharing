fa107007eac1951cf5cdfb77cc7ff916c1060602
Yevhwnii <mirandoos3223@gmail.com>
2020-08-15 16:36:51 +0300

    adjustment

--- frontend/src/shared/components/FormElements/Input.js
+++ frontend/src/shared/components/FormElements/Input.js
@@ -26,9 +26,9 @@ const Input = (props) => {
   // useReducer is more complex state managment than useState
   // allows to do something whenever state changes and so on
   const [inputState, dispatch] = useReducer(inputReducer, {
-    value: props.value || "",
+    value: props.initialValue || "",
     isTouched: false,
-    isValid: props.valid || false,
+    isValid: props.initialValid || false,
   }); // inital values of the state
 
   const { id, onInput } = props;
