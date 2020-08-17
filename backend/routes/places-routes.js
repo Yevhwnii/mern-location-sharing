ecf96b98e2df99d67e92fe746493987e034d7e46
Yevhwnii <mirandoos3223@gmail.com>
2020-08-17 16:46:08 +0300

    update and delete place routes

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -10,6 +10,13 @@ router.get("/user/:userId", placeController.getPlaceByUserId);
 // GET => /api/places/:pid => get list of all places
 router.get("/:placeId", placeController.getPlaceById);
 
+// POST => /api/place/:pid => create new place
 router.post("/", placeController.createPlace);
+
+// PATCH => /api/place/:pid => update place
+router.patch("/:placeId", placeController.updatePlace);
+
+// DELETE => /api/place/:pid => delete place
+router.delete("/:placeId", placeController.deletePlace);
 
 module.exports = router;
