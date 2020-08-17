84d77b1f93afcfa563da451cb0f85678826d4b4a
Yevhwnii <mirandoos3223@gmail.com>
2020-08-17 15:59:43 +0300

    place routes with dummy data

---
+++ backend/routes/places-routes.js
@@ -0,0 +1,61 @@
+const express = require("express");
+
+const router = express.Router();
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "Famous skyscraper",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th St, New York, NY 10001, United States",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9856644,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "Famous skyscraper",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th St, New York, NY 10001, United States",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9856644,
+    },
+    creator: "u2",
+  },
+];
+
+// GET => /api/places/user/:pid => get list of places for user
+router.get("/user/:userId", (req, res, next) => {
+  const userId = req.params.userId;
+
+  const place = DUMMY_PLACES.find((p) => p.creator === userId);
+
+  if (!place) {
+    const error = new Error("Could not find requested place for such user id");
+    error.code = 404;
+    return next(error);
+  }
+  res.json({ place });
+});
+
+// GET => /api/places/:pid => get list of all places
+router.get("/:placeId", (req, res, next) => {
+  const placeId = req.params.placeId;
+  const place = DUMMY_PLACES.find((p) => p.id === placeId);
+
+  if (!place) {
+    const error = new Error("Could not find requested place for such place id");
+    error.code = 404;
+    return next(error);
+  }
+  res.json({ place });
+});
+
+module.exports = router;
