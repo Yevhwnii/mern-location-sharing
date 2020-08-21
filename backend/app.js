77b5987c217534cd34aec4b2b24c738e39d48fea
Yevhwnii <mirandoos3223@gmail.com>
2020-08-21 14:45:49 +0300

    handling cors errors

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,17 @@ const MONGO_URI = process.env.MONGO_URI;
 
 // Middlewares
 app.use(bodyParser.json());
+
+// CORS handling middleware
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Origin, X-Requested-With, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
 // Routes
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
