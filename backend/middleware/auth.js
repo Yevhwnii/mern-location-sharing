14239165de819fb754e2a466bdfb7b775c5ca9ee
Yevhwnii <mirandoos3223@gmail.com>
2020-08-25 15:16:32 +0300

    auth check middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const HttpError = require("../models/http-error");
+
+const JWTSECRET = process.env.SECRET_JWT;
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      throw new HttpError("Auth failed!", 401);
+    }
+    const decodedToken = jwt.verify(token, JWTSECRET);
+    req.userData = { userId: decodedToken.userId };
+    next();
+  } catch (error) {
+    return next(new HttpError("Authentication failed!", 401));
+  }
+};
