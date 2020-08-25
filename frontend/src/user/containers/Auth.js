be622a396f9babbbae69cf6c6b021443c29a4ea9
Yevhwnii <mirandoos3223@gmail.com>
2020-08-25 15:30:30 +0300

    adjusting response data from backend

--- frontend/src/user/containers/Auth.js
+++ frontend/src/user/containers/Auth.js
@@ -52,7 +52,7 @@ const Auth = (props) => {
             "Content-Type": "application/json",
           }
         );
-        auth.login(responseData.user.id);
+        auth.login(responseData.userId, responseData.token);
       } catch (error) {
         console.log(error);
       }
@@ -68,7 +68,7 @@ const Auth = (props) => {
           "POST",
           formData
         );
-        auth.login(responseData.user.id);
+        auth.login(responseData.userId, responseData.token);
       } catch (error) {
         console.log(error);
       }
