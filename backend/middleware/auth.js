751daadd967d4e81e30b10a1582a31f733b641a1
Yevhwnii <mirandoos3223@gmail.com>
2020-08-25 15:29:27 +0300

    allowing options type of req pass thru

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,6 +5,9 @@ const HttpError = require("../models/http-error");
 const JWTSECRET = process.env.SECRET_JWT;
 
 module.exports = (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
   try {
     const token = req.headers.authorization.split(" ")[1];
     if (!token) {
