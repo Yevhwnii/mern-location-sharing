747a4e9bd00ef34c7f7999a5d50f500043cdb518
Yevhwnii <mirandoos3223@gmail.com>
2020-08-26 14:48:37 +0300

    adding port env var

--- backend/app.js
+++ backend/app.js
@@ -58,7 +58,7 @@ mongoose
   .connect(MONGO_URI)
   .then(() => {
     console.log("MongoDb is connected");
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
     console.log("Server is running on port 5000");
   })
   .catch((err) => {
